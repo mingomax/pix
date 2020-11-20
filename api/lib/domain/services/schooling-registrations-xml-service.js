5d3799118cfb423e15ff5da3f8d4aa8bc52f2b4f
ajoanny <ajoanny@octo.com>
2020-11-20 14:43:15 +0100

    [API] refacto extract XMLStreamer in another file

--- api/lib/domain/services/schooling-registrations-xml-service.js
+++ api/lib/domain/services/schooling-registrations-xml-service.js
@@ -1,17 +1,9 @@
 const { FileValidationError, SameNationalStudentIdInFileError } = require('../errors');
-const fs = require('fs');
-const StreamZip = require('node-stream-zip');
-const FileType = require('file-type');
-const iconv = require('iconv-lite');
 const moment = require('moment');
 const xml2js = require('xml2js');
-const sax = require('sax');
 const saxPath = require('saxpath');
-const xmlEncoding = require('xml-buffer-tostring').xmlEncoding;
 const { isEmpty, isNil, each, isUndefined } = require('lodash');
-const readline = require('readline');
 
-const DEFAULT_FILE_ENCODING = 'iso-8859-15';
 const DIVISION = 'D';
 const NODE_ORGANIZATION_UAI = '/BEE_ELEVES/PARAMETRES';
 const NODES_SCHOOLING_REGISTRATIONS = '/BEE_ELEVES/DONNEES/*/*';
@@ -20,101 +12,16 @@ const STRUCTURE_ELEVE_ELEMENT = '<STRUCTURES_ELEVE';
 const NO_STUDENTS_IMPORTED_FROM_INVALID_FILE = 'Aucun élève n’a pu être importé depuis ce fichier. Vérifiez que le fichier est conforme.';
 const UAI_SIECLE_FILE_NOT_MATCH_ORGANIZATION_UAI = 'Aucun étudiant n’a été importé. L’import n’est pas possible car l’UAI du fichier SIECLE ne correspond pas à celui de votre établissement. En cas de difficulté, contactez support.pix.fr.';
 
-const Stream = require('stream');
+const XMLStreamer = require('../../infrastructure/utils/xml/xml-streamer');
 
-class StreamPipe extends Stream.Transform {
-
-  constructor() {
-    super();
-  }
-
-  _transform(chunk, enc, cb) {
-    this.push(chunk);
-    cb();
-  }
-}
-
-function _unzippedStream(path) {
-  const zip = new StreamZip({ file: path });
-  const stream = new StreamPipe();
-
-  zip.on('entry', (entry) => {
-    zip.stream(entry, (err, stm) => {
-
-      if (!entry.name.includes('/')) {
-        stm.pipe(stream);
-      }
-    });
-  });
-  return stream;
-}
-
-const ZIP = 'zip';
-
-class XMLStreamer {
-  static async create(path) {
-    const stream = new XMLStreamer(path)
-    await stream._detectEncoding();
-
-    return stream;
-  }
-
-  constructor(path) {
-    this.path = path;
-  }
-
-  async _detectEncoding() {
-    const firstLine = await this._readFirstLine();
-    this.encoding = xmlEncoding(Buffer.from(firstLine)) || DEFAULT_FILE_ENCODING;
-  }
-
-  async getStream(){
-    this.stream = await this._getRawStream();
-    const saxParser = sax.createStream(true);
-    return this.stream.pipe(iconv.decodeStream(this.encoding)).pipe(saxParser);
-  }
-
-  async _getRawStream(){
-    let stream;
-    if (await this._isFileZipped()) {
-      stream = _unzippedStream(this.path);
-    }
-    else {
-      stream = fs.createReadStream(this.path);
-    }
-    return stream;
-  }
-
-  async _isFileZipped() {
-    const { ext } = await FileType.fromFile(this.path);
-    return ext === ZIP
-  }
-
-  async _readFirstLine() {
-    const stream = await this._getRawStream();
-    const firstLineReader = readline.createInterface({ input: stream });
-
-    return await new Promise((resolve) => {
-      firstLineReader.on('line', (line) => {
-        firstLineReader.close();
-        resolve(line);
-      })
-    });
-  }
-
-  destroyStream() {
-    this.stream.destroy();
-  }
-}
-
-let XMLstreamer;
+let XmlStreamer;
 
 module.exports = {
   extractSchoolingRegistrationsInformationFromSIECLE,
 };
 
 async function extractSchoolingRegistrationsInformationFromSIECLE(path, organization) {
-  XMLstreamer = await XMLStreamer.create(path)
+  XmlStreamer = await XMLStreamer.create(path)
 
   const UAIFromSIECLE = await _extractUAI(path);
   const UAIFromUserOrganization = organization.externalId;
@@ -137,12 +44,12 @@ async function _processSiecleFile() {
 }
 
 async function _withSiecleStream(fn) {
-  const siecleFileStream = await XMLstreamer.getStream();
+  const siecleFileStream = await XmlStreamer.getStream();
 
   try {
     return await fn(siecleFileStream);
   } finally {
-    XMLstreamer.destroyStream();
+    XmlStreamer.destroyStream();
   }
 }
 
